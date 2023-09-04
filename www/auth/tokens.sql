71dca028df5a3620c76667d233901e4bd4c961c1
Jimmy Remy <jimmy@itbyjimmy.com>
2023-09-04 12:41:23 -0700

    clean slate

--- www/auth/tokens.sql
+++
@@ -1,8 +0,0 @@
--- requires the users table to be already created
-
-CREATE TABLE `tokens` (
-  `token` varchar(256) NOT NULL PRIMARY KEY,
-  `username` varchar(32) NOT NULL REFERENCES users(username) ON UPDATE CASCADE ON DELETE CASCADE,
-  `access` varchar(64),
-  `expiration` int(10) NOT NULL COMMENT 'unix epoch time'
-) ENGINE=InnoDB;

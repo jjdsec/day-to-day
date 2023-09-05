1f6544e54b9d4b68ebfa4d1b43934a9b53c7dffa
Jimmy Remy <jimmy@itbyjimmy.com>
2023-09-04 18:19:50 -0700

    corrected title variable

--- www-html/inc/header.php
+++ www-html/inc/header.php
@@ -3,6 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php if (isset($page_title)) echo "$page_title"; else echo "Empty Title" ?></title>
+    <title><?php if (isset($page['title'])) echo $page['title']; else echo "Empty Title" ?></title>
 </head>
 <body>
